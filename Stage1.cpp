68abfbdd54cffdffab2c893249c9b30ee2c904bc
Sehee-Lee-01 <tpfktpgml24@kookmin.ac.kr>
2022-06-10 20:45:18 +0900

    stg1 fin

--- Stage1.cpp
+++ Stage1.cpp
@@ -1,6 +1,8 @@
 #include<ncurses.h>
 #include<fstream>
 #include<iostream>
+#include <vector>
+
 
 using namespace std;
 // Size define
@@ -8,12 +10,16 @@ using namespace std;
 #define map_width 42
 ifstream map_file;
 int map[map_height][map_width]; // Map
+//snake
+vector<int> snake_x;
+vector<int> snake_y; 
+
 void show_map()
 {
     init_pair(1, COLOR_WHITE, COLOR_WHITE); // 0: background
     init_pair(2, COLOR_CYAN, COLOR_CYAN); // 1: wall
     init_pair(3, COLOR_BLACK, COLOR_BLACK); // 2: Immune Wall
-    string filename = "./snakemap/snakemap4.txt";
+    string filename = "./snakemap/snakemap1.txt";
     map_file.open(filename);
     string line;
     for (int i=0; i<map_height; i++)
@@ -46,13 +52,43 @@ void show_map()
     map_file.close();
     refresh();
 }
-
+void show_snake()
+{
+    init_pair(4, COLOR_YELLOW, COLOR_YELLOW); // snake head
+    init_pair(5, COLOR_GREEN, COLOR_GREEN); // snake body
+    // print head
+    attron(COLOR_PAIR(4));
+    mvprintw(snake_x[0], snake_y[0]," ");
+    attroff(COLOR_PAIR(4));
+ 
+    //print body 
+    for(int i=1; i<snake_x.size();i++)
+    {
+        attron(COLOR_PAIR(5));
+        mvprintw(snake_x[i], snake_y[i]," ");
+        attroff(COLOR_PAIR(5));
+    }
+    refresh();
+}
 int main()
 {
+    // init snake place
+    for (int i=0; i<4; i++)
+    {    
+        snake_x.push_back(11); 
+        snake_y.push_back(11+i);
+    }
+
     initscr();
+    resize_term(21, 42);
     start_color();
+    init_pair(6, COLOR_GREEN, COLOR_GREEN); // background
+    bkgd(COLOR_PAIR(6));
 
     show_map();
+    // getch();
+
+    show_snake();
 	getch();
 	endwin();
 
